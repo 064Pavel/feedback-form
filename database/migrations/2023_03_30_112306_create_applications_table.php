489f6ec8f3ffab4cf35c9e5d68834dadee5ef834
LelouchLamperougee <pavel.bv2003@gmail.com>
2023-03-31 23:40:50 +0300

    Changed tables

--- database/migrations/2023_03_30_112306_create_applications_table.php
+++ database/migrations/2023_03_30_112306_create_applications_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('user_name');
             $table->string('title');
             $table->text('description');
+            $table->boolean('is_active')->default(false);
 
             $table->foreignId('user_id')->constrained()->cascadeOnDelete()->cascadeOnUpdate();
 
